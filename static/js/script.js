5f39c704d05b35093cad41d7af54c71f4950e584
huafeng2019 <50068769+huafeng2019@users.noreply.github.com>
2022-01-01 00:04:30 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -54,7 +54,7 @@ $(function(){
   })
 
   $('.countdown').countdown({
-    until: new Date('January, 1, 2022') // you can add time optionally ('January, 1, 2022 00:00:00')
+    until: new Date('January, 1, 2023') // you can add time optionally ('January, 1, 2023 00:00:00')
   })
   $('.snow').snow({
     amount: 100 //Number of particles
